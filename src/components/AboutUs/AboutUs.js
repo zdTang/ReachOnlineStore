952d3f1e3402a4cc9982f54d8a18e5e65f7ef3db
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-02-06 10:10:46 -0500

    Add yml file and build project

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      <h1>[Page Title] - Coming Soon!</h1>
+      <p>I am working on the full version of this page.</p>
+      <p>I appreciate your patience. Check back in a few weeks!</p>
+    </div>
+  );
 };
 
 export default AboutUs;
--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div>
+      <h1>Coming Soon!</h1>
+      <p>
+        I am working on the full version of this page, but here's a preview of
+        what you can expect:
+      </p>
+      <ul>
+        <li>
+          {" "}
+          Product Data Management: A user-friendly interface for administrators
+          to easily update and maintain detailed product information.
+        </li>
+        <li>
+          {" "}
+          Profit Analytics Dashboard: Interactive charts and reports providing
+          clear visualizations of profit performance.
+        </li>
+        <li>
+          Customizable Reporting Periods: The ability to analyze profit outcomes
+          over various time periods
+        </li>
+      </ul>
+      <p>I appreciate your patience. Check back in a few weeks!</p>
+    </div>
+  );
 };
 
 export default Settings;
