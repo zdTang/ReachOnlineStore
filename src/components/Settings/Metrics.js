bbbb23f91fa1d535645ac7f54beb79088da7626d
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2025-02-09 14:27:54 -0500

    added a line chart

--- src/components/Settings/Metrics.js
+++ src/components/Settings/Metrics.js
@@ -1,16 +1,89 @@
-import Card from "../UI/Card";
-import UnderDevelopment from "../UI/UnderDevelopment";
-const Metrics = () => {
-  return (
-    <section className="max-w-[60rem] w-[90%] my-8  mx-auto animate-meals-appear">
-      <Card>
-        <ul className="list-none m-0 p-0">
-          <h1>Tables and charts</h1>
-          <UnderDevelopment />
-        </ul>
-      </Card>
-    </section>
-  );
-};
+import React, { PureComponent } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
-export default Metrics;
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+export default class Metrics extends PureComponent {
+  render() {
+    return (
+      <div class="container mx-auto max-w-full">
+        <LineChart
+          width={1000}
+          height={600}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </div>
+    );
+  }
+}
